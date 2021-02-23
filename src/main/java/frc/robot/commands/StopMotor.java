31b05621fff7aea65225953f958b2ed3af3d63e0
Alexedw01 <Alexedw01@icloud.com>
2021-02-22 21:10:45 -0800

    error fixes

--- src/main/java/frc/robot/commands/StopMotor.java
+++ src/main/java/frc/robot/commands/StopMotor.java
@@ -29,6 +29,6 @@ public class StopMotor extends CommandBase {
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return true;
+    return false;
   }
 }
